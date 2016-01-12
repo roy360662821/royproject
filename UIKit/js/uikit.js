846138dbc19a27d16c2a55adfe23dcd1b8ed7140
Administrator <roy360662821@hotmail.com>
2016-01-12 18:00:04 +0800

    Update kit

--- UIKit/js/uikit.js
+++ UIKit/js/uikit.js
@@ -18,7 +18,6 @@ angular.module('myApp').controller('uikit', function($scope) {
     		var sliImg=sliImgArr[num];
     		$("."+sliImg).slideUp(100);
     		num++;
-    		console.log(num+" next");
     	}
     });
     
@@ -28,7 +27,6 @@ angular.module('myApp').controller('uikit', function($scope) {
     		var sliImg=sliImgArr[num-1];
     		$("."+sliImg).slideDown(200);
     		num--;
-    		console.log(num+" last");
     	}
     });
     
@@ -63,11 +61,6 @@ angular.module('myApp').controller('uikit', function($scope) {
 		$(".dropdownquery_options").slideUp(200); 
 	}); 
     
-    $(".form_control i").click(function(){
-    	$(this).attr("class","icon-radio-checked");
-    	$(this).siblings("i").attr("class","icon-radio-unchecked");
-    });
-    
     //Choice a item
     $(".dropdownquery_options div").click(function(){
     	var inputText=$(this).parent().prev().find("input").first();
@@ -75,4 +68,182 @@ angular.module('myApp').controller('uikit', function($scope) {
     	$(this).parent().slideUp(200);
     });
     
+    //select radio btn or checkbox btn
+    $(".form_control i").click(function(){
+    	if($(this).attr("class") == "icon-radio-unchecked"){
+    		$(this).attr("class","icon-radio-checked");
+    		$(this).siblings("i").attr("class","icon-radio-unchecked");
+    	}
+    	if($(this).attr("class") == "icon-checkbox-unchecked") $(this).attr("class","icon-checkbox-checked");
+    	else if($(this).attr("class") == "icon-checkbox-checked") $(this).attr("class","icon-checkbox-unchecked");
+    });
+    
+    //calender control
+    
+    
+    //create calender control
+    $(".calender_controller_btn").click(function(){
+    	if($(".calender_controller_panel").length>0){
+    		$(".calender_controller_panel").slideToggle(200);
+    		$(".calender_controller_panel").remove();
+    	}else{
+    		var nowDate=new Date();			//当前日期
+			var nowDay=nowDate.getDate();	//当前几号
+			var nowMonth=nowDate.getMonth();//当前月份
+			var nowYear=nowDate.getFullYear();  //当前年份
+			//判断当月一号星期几
+			var newDate=new Date(nowYear,nowMonth,1);
+			var weekDayOne=newDate.getDay();
+			var monthDays=getMonthDays(nowYear,nowMonth+1);
+			var dayLength=35;
+			if(weekDayOne+monthDays>dayLength){
+				dayLength=42;
+			}
+			var daysInnerHtml="";
+			for(var i=1;i<=dayLength;i++){
+				if(i<=weekDayOne){
+					daysInnerHtml=daysInnerHtml+"<div></div>";
+				}else{
+					if(i-weekDayOne<=monthDays){
+						if(i-weekDayOne == nowDay) daysInnerHtml=daysInnerHtml+"<div onclick='selectDays(this)' class='calender_controller_days calender_controller_nowday'>"+(i-weekDayOne)+"</div>"
+						else daysInnerHtml=daysInnerHtml+"<div onclick='selectDays(this)' class='calender_controller_days'>"+(i-weekDayOne)+"</div>";
+					}else{
+						daysInnerHtml=daysInnerHtml+"<div></div>";
+					}
+				}
+			}
+			var calenderPanel=$("<div></div>");
+			$(this).parent().parent().append(calenderPanel);
+			calenderPanel.attr("class","calender_controller_panel");
+			var calenderPanelHtmlOne="<div class=\"calender_controller_panel_head\"></div>"
+							+"<div class=\"calender_controller_panel_month_year_choice\">"
+								+"<div class=\"calender_controller_last_month\" onclick='lastMonthClick()'>"
+									+"<div class=\"triangle-left\"></div>"
+								+"</div>"
+								+"<div class=\"calender_controller_month_year\">"
+									+"<div class=\"calender_controller_month\">January</div>"
+									+"<div class=\"calender_controller_year\">2016</div>"
+								+"</div>"
+								+"<div class=\"calender_controller_next_month\" onclick='nextMonthClick()'>"
+									+"<div class=\"triangle-right\"></div>"
+								+"</div>"
+							+"</div>"
+							+"<div class=\"calender_controller_date_panel\" align=\"center\">"
+								+"<div class=\"calender_controller_week\">"
+									+"<div>SUN</div>"
+									+"<div>MON</div>"
+									+"<div>TUE</div>"
+									+"<div>WED</div>"
+									+"<div>THU</div>"
+									+"<div>FRI</div>"
+									+"<div>SAT</div>"
+								+"</div>"
+								+"<div class=\"calender_controller_days_panel\">";
+			var calenderPanelHtmlTwo="</div></div>";
+			var calenderPanelHtml=calenderPanelHtmlOne+daysInnerHtml+calenderPanelHtmlTwo;
+			calenderPanel.html(calenderPanelHtml);
+			if(dayLength!=42) $(".calender_controller_days_panel").css("height","150px");
+			else $(".calender_controller_days_panel").css("height","180px");
+	    	$(".calender_controller_panel").slideToggle(200);
+    	}
+    });
+    
 });
+
+//check month days
+function getMonthDays(year,month){
+	if(month == 2){
+		if (((year % 4)==0) && ((year % 100)!=0) || ((year % 400)==0))  return 29;
+		else return 28;
+	}
+	console.log("year"+year);
+	console.log("month"+month);
+	switch (month){
+		case 1:
+			return 31;
+		case 3:
+			return 31;
+		case 5:
+			return 31;
+		case 7:
+			return 31;
+		case 8:
+			return 31;
+		case 10:
+			return 31;
+		case 12:
+			return 31;
+		case 4:
+			return 30;
+		case 6:
+			return 30;
+		case 9:
+			return 30;
+		case 11:
+			return 30;
+		default:
+			break;
+	}
+}
+
+var monthArr={January:0,February:1,March:2,April:3,May:4,June:5,July:6,August:7,September:8,October:9,November:10,December:11};
+function selectDays(obj){
+	$(".calender_controller_text").html($(".calender_controller_year").html()+"-"+(monthArr[$(".calender_controller_month").html()]+1)+"-"+obj.innerHTML);
+	$(".calender_controller_panel").slideToggle(200);
+	$(".calender_controller_panel").remove();
+}
+
+function lastMonthClick(){
+	var month=monthArr[$(".calender_controller_month").html().trim()];
+	var year=$(".calender_controller_year").html().trim();
+	if(parseInt(month)==0){
+		month=11;
+		year=parseInt(year)-1;
+	}else{
+		month=parseInt(month)-1;
+	}
+	reloadCalenderPanel(month,year);
+}
+
+function nextMonthClick(){
+	var month=monthArr[$(".calender_controller_month").html().trim()];
+	var year=$(".calender_controller_year").html().trim();
+	if(parseInt(month)==11){
+		month=0;
+		year=parseInt(year)+1;
+	}else{
+		month=parseInt(month)+1;
+	}
+	reloadCalenderPanel(month,year);
+}
+
+function reloadCalenderPanel(month,year){
+	$(".calender_controller_year").html(year);
+	for(var mon in monthArr){
+		if(monthArr[mon] == month){
+			$(".calender_controller_month").html(mon);
+		}
+	}
+	var newDate=new Date(year,month,1);
+	var weekDayOne=newDate.getDay();
+	var monthDays= getMonthDays(year,(month+1));
+	var dayLength=35;
+	if(weekDayOne+monthDays>dayLength){
+		dayLength=42;
+	}
+	var daysInnerHtml="";
+	for(var i=1;i<=dayLength;i++){
+		if(i<=weekDayOne){
+			daysInnerHtml=daysInnerHtml+"<div></div>";
+		}else{
+			if(i-weekDayOne<=monthDays){
+				daysInnerHtml=daysInnerHtml+"<div onclick='selectDays(this)' class='calender_controller_days'>"+(i-weekDayOne)+"</div>";
+			}else{
+				daysInnerHtml=daysInnerHtml+"<div></div>";
+			}
+		}
+	}
+	if(dayLength!=42) $(".calender_controller_days_panel").css("height","150px");
+	else $(".calender_controller_days_panel").css("height","180px");
+	$(".calender_controller_days_panel").html(daysInnerHtml);
+}
